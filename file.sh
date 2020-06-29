b2f9602ba5f45c07897c3231e18127ae1c977ab5
Kaira Hiwatari <46723211+anshukaira@users.noreply.github.com>
2020-06-29 22:53:14 +0530

    Create file.sh

---
+++ file.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#This file listing method does not uses any pre defined command.
+
+echo "Welcome to OS project"
+
+echo "Here is the list of files present in the current directory"
+
+for FILE in *
+do 
+    echo $FILE 
+done
+
