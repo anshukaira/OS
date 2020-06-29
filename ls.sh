3c9adff310f2cabfdcd2d99e9d66cfd206c62bf4
Kaira Hiwatari <46723211+anshukaira@users.noreply.github.com>
2020-06-29 23:47:03 +0530

    Create ls.sh
    Using ls, this method will let us not only give us list of current dir file names but list of all the files which are inside the folders of the current directory

---
+++ ls.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# loop over files, but using a pipe (reading from standard input)
+#the output will show all the files and folders and files within those folder
+
+echo "Welcome to OS project"
+
+echo "Here is the list of files present in the current directory"
+
+ls * 
